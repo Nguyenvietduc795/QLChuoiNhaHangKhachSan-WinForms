a1a79e017425e394cd9826f7d08fb29ab8ade746
TruongPhi05 <ntp160405@gmail.com>
2025-12-19 20:51:49 +0700

    Mô phỏng chức năng booking

--- QLChuoiNhaHangKhachSan.GUI/Program.cs
+++ QLChuoiNhaHangKhachSan.GUI/Program.cs
@@ -16,7 +16,7 @@ namespace QLChuoiNhaHangKhachSan.GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormInventory());
+            Application.Run(new FormBookinng());
 
         }
     }
